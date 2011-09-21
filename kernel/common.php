0fa6336008b3913c619b3b0db23cf55cd45f0df9
Zeuxis Lo <seekstudio@gmail.com>
2011-09-21 14:45:39 +0800

    added common function file

---
+++ kernel/common.php
@@ -0,0 +1,15 @@
+<?php
+if (defined('IN_APP') === false) exit('Access Dead');
+
+spl_autoload_register("auto_load");
+
+function auto_load($class_name) {
+	foreach(array('core', 'helper', 'library') as $folder) {
+		$file_path = KERNEL_ROOT.'/'.$folder.'/'.strtolower($class_name).'.php';
+
+		if (file_exists($file_path) === true) {
+			require_once $file_path;
+		}
+	}
+}
+?>
