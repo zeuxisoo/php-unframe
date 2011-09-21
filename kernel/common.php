02b3e73ecf014d1825b114b398bc258901288b9e
Zeuxis Lo <seekstudio@gmail.com>
2011-09-21 17:54:17 +0800

    added autoload on adapter/database folder

--- kernel/common.php
+++ kernel/common.php
@@ -4,7 +4,7 @@ if (defined('IN_APP') === false) exit('Access Dead');
 spl_autoload_register("auto_load");
 
 function auto_load($class_name) {
-	foreach(array('core', 'helper', 'library') as $folder) {
+	foreach(array('core', 'helper', 'library', 'adapter/database') as $folder) {
 		$file_path = KERNEL_ROOT.'/'.$folder.'/'.strtolower($class_name).'.php';
 
 		if (file_exists($file_path) === true) {
