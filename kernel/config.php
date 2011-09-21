528f2a5829becdd382b776e76bc5c70267155497
Zeuxis Lo <seekstudio@gmail.com>
2011-09-21 14:45:10 +0800

    added global config

---
+++ kernel/config.php
@@ -0,0 +1,14 @@
+<?php
+if (defined('IN_APP') === false) exit('Access Dead');
+
+$config = array(
+	'init' => array(
+		'site_url' 			=> 'http://localhost/labs/unframework',
+		'attachment_folder'	=> 'attachment',
+		'admin_folder'		=> 'admin',
+		'timezone'			=> 'Asia/Hong_Kong',
+		'header_no_cache'	=> true,
+		'show_php_error'	=> true,
+	),
+);
+?>
