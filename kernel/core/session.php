865af18a85c334ca660b28e138abfa5301738502
Zeuxis Lo <seekstudio@gmail.com>
2011-09-21 14:47:24 +0800

    added session core file

---
+++ kernel/core/session.php
@@ -0,0 +1,30 @@
+<?php
+if (defined('IN_APP') === false) exit('Access Dead');
+
+class Session {
+	
+	private static $instance = null;
+
+	public static function init() {
+		if (self::$instance === null) {
+			self::$instance = new Session();
+			session_start();
+		}
+	}
+
+	public static function set($name, $value) {
+		$_SESSION[$name] = $value;
+	}
+
+	public static function get($name, $clear = false) {
+		$value = Request::session($name);
+
+		if (isset($_SESSION[$name]) === true && $clear === true) {
+			unset($_SESSION[$name]);
+		}
+
+		return $value;
+	}
+
+}
+?>
