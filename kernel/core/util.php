2f7e65a94859cf42f239d228de1d495ee5255211
Zeuxis Lo <seekstudio@gmail.com>
2011-09-22 20:04:59 +0800

    moved to_date_time to clock core, add_slash to secure core

--- kernel/core/util.php
+++ kernel/core/util.php
@@ -3,22 +3,7 @@ if (defined('IN_APP') === false) exit('Access Dead');
 
 class Util {
 	
-	public static function add_slash($string, $force = 0, $strip = false) {
-		if(get_magic_quotes_gpc() == false || $force) {
-			if(is_array($string)) {
-				foreach($string as $key => $val) {
-					$string[$key] = self::add_slash($val, $force, $strip);
-				}
-			} else {
-				$string = addslashes($strip === true ? stripslashes($string) : $string);
-			}
-		}
-		return $string;
-	}
-
-	public static function to_date_time($timestamp, $format = 'Y-m-d', $time_zone = 8) {
-		return gmdate($format, $timestamp + $time_zone * 3600);
-	}
-
+	
+	
 }
 ?>
