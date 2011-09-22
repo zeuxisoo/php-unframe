d4d03d6c91c443609a8b5e74cec1df4b1b47a14d
Zeuxis Lo <seekstudio@gmail.com>
2011-09-22 23:19:24 +0800

    added cookie core file

---
+++ kernel/core/cookie.php
@@ -0,0 +1,23 @@
+<?php
+if (defined('IN_APP') === false) exit('Access Dead');
+
+class Cookie {
+	
+	public static function get($name) {
+		return Request::cookie($name);
+	}
+
+	public static function set($name, $value, $time_out = 3600, $path = '/', $domain = '') {
+		setcookie($name, $value, $time_out, $path, $domain, ($_SERVER['SERVER_PORT'] == 443 ? 1 : 0));
+	}
+
+	public static function remove($name) {
+		self::add($name, '', -84600);
+
+		if (isset($_COOKIE[$name])) {
+			unset($_COOKIE[$name]);
+		}
+	}
+
+}
+?>
