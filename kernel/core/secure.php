57f2a266396b16b9d34b631868d2d2135f12f7a3
Zeuxis Lo <seekstudio@gmail.com>
2011-09-22 20:04:03 +0800

    added secure core file and moved add_slash method in

---
+++ kernel/core/secure.php
@@ -0,0 +1,20 @@
+<?php
+if (defined('IN_APP') === false) exit('Access Dead');
+
+class Secure {
+
+	public static function add_slash($string, $force = 0, $strip = false) {
+		if(get_magic_quotes_gpc() == false || $force) {
+			if(is_array($string)) {
+				foreach($string as $key => $val) {
+					$string[$key] = self::add_slash($val, $force, $strip);
+				}
+			} else {
+				$string = addslashes($strip === true ? stripslashes($string) : $string);
+			}
+		}
+		return $string;
+	}
+	
+}
+?>
