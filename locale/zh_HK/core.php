6ea36d7c70778ebf9e69b2a60890bb9755c09de1
Zeuxis Lo <seekstudio@gmail.com>
2011-09-22 20:52:24 +0800

    added language parts for human_time method

---
+++ locale/zh_HK/core.php
@@ -0,0 +1,10 @@
+<?php
+if (defined('IN_APP') === false) exit('Access Dead');
+
+$lang = array(
+	'%{limit} second ago' => '%{limit} 秒前',
+	'%{miute} minute %{second} second ago' => '%{miute} 分 %{second} 秒前',
+	'%{hour} hour %{minute} minute ago' => '%{hour} 時 %{minute} 分前',
+	'%{day} day ago' => '%{day} 天前'
+);
+?>
