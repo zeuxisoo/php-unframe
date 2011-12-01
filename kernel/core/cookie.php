03961f5adc66372b5db299a27660f90120607584
Zeuxis Lo <seekstudio@gmail.com>
2011-12-01 16:48:05 +0800

    added url::schema method for find out current site url

--- kernel/core/cookie.php
+++ kernel/core/cookie.php
@@ -2,12 +2,25 @@
 if (defined('IN_APP') === false) exit('Access Dead');
 
 class Cookie {
-	
+
 	public static function get($name) {
 		return Request::cookie($name);
 	}
 
 	public static function set($name, $value, $time_out = 3600, $path = '/', $domain = '') {
+		if (empty($domain) === true) {
+			$domain = $_SERVER['HTTP_HOST'];
+		}
+
+		if (in_array($domain, array('localhost', '127.0.0.1')) === false && self::get_client_ip() != "127.0.0.1") {
+			if (strtolower(substr($domain, 0, 4)) == 'www.') {
+				$domain = substr($domain, 4);
+			}
+	      	$domain = '.'.$domain;
+      	}else{
+      		$domain = "";
+      	}
+
 		setcookie($name, $value, $time_out, $path, $domain, ($_SERVER['SERVER_PORT'] == 443 ? 1 : 0));
 	}
 
